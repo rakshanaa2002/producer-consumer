ac88ea34f1f6d4b76742171fed833b59237fd7f9
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-03-26 11:27:04 +0530

    share.java

--- share.java
+++ share.java
@@ -1,22 +1,35 @@
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 public class share {
     LinkedList<Integer> data = new LinkedList<>();
-    public synchronized void pro(int value){
+    public synchronized void pro(int value) throws InterruptedException{
         data.add(value);
         notify();
     }
     public synchronized int con() throws InterruptedException{
         while(data.size()==0)
             wait();
-        return data.removeFirst();
+        Random a=new Random();
+        int i=a.nextInt(5);
+        notify();
+        return data.get(i);
     }
     public static void main(String[] args) throws InterruptedException {
         share s=new share();
-        producer p = new producer(s);
-        consumer c = new consumer(s);
-        p.start();
-        c.start();
-        p.join();
-        c.join();
+        int p = 5;
+        int c = 10;
+        ExecutorService pe = Executors.newFixedThreadPool(p);
+        ExecutorService ce = Executors.newFixedThreadPool(c);
+        for (int i = 0; i < p; i++) {
+            pe.submit(new producer(s));
+        }
+        for (int i = 0; i < c; i++) {
+            ce.submit(new consumer(s));
+        }
+
+        pe.shutdown();
+        ce.shutdown();
     }
 }
