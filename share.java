a489dc45eebabae17141f99e1aa5030b54209c6b
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-03-27 10:20:03 +0530

    share.java

--- share.java
+++ share.java
@@ -1,34 +1,17 @@
 import java.util.LinkedList;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 public class share {
-    LinkedList<Integer> data = new LinkedList<>();
-    public synchronized void pro(int value) throws InterruptedException{
-        data.add(value);
-        notify();
-    }
-    public synchronized int con() throws InterruptedException{
-        while(data.size()==0)
-            wait();
-        Random a=new Random();
-        int i=a.nextInt(5);
-        notify();
-        return data.get(i);
-    }
-    public static void main(String[] args) throws InterruptedException {
-        share s=new share();
-        int p=5;
-        int c=10;
-        ExecutorService pe =Executors.newFixedThreadPool(p);
-        ExecutorService ce =Executors.newFixedThreadPool(c);
-        for (int i=0;i<p;i++) {
-            pe.submit(new producer(s));
+    LinkedList<Integer> data=new LinkedList<>();    
+    public static void main(String[] args)throws InterruptedException {
+        share share=new share();
+        int producerNum=5;
+        int consumeNum=6;
+        for(int i=1;i<=producerNum;i++){
+            producer producer=new producer(share, i);
+            producer.start();
         }
-        for (int i=0;i<c;i++) {
-            ce.submit(new consumer(s));
+        for(int i=1;i<=consumeNum;i++){
+            consumer consumer=new consumer(share, i);
+            consumer.start();
         }
-        pe.shutdown();
-        ce.shutdown();
     }
 }
