b2f2c21cbfbd99ab2ad8ed2ab894370a5c1f57b9
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-03-28 16:22:16 +0530

    share.java

--- share.java
+++ share.java
@@ -1,16 +1,20 @@
-import java.util.LinkedList;
-public class share {
-    LinkedList<Integer> data=new LinkedList<>();    
-    public static void main(String[] args)throws InterruptedException {
+import java.util.concurrent.ArrayBlockingQueue;
+public class share
+{
+    ArrayBlockingQueue<Integer> data = new ArrayBlockingQueue<>(10);
+    public static void main(String[] args)
+    {
         share share=new share();
         int producerNum=5;
-        int consumeNum=6;
-        for(int i=1;i<=producerNum;i++){
-            producer producer=new producer(share, i);
+        int consumerNum=6;
+        for (int i=1;i<=producerNum;i++)
+        {
+            producer producer=new producer(share,i);
             producer.start();
         }
-        for(int i=1;i<=consumeNum;i++){
-            consumer consumer=new consumer(share, i);
+        for (int i=1;i<=consumerNum;i++)
+        {
+            consumer consumer=new consumer(share,i);
             consumer.start();
         }
     }
