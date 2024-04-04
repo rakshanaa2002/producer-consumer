89155e1160265817047b76871d56b23a1f62a4fb
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-04-04 13:19:51 +0530

    producer.java

--- producer.java
+++ producer.java
@@ -1,34 +1,41 @@
 import java.util.Random;
-public class producer extends Thread
-{
+
+public class producer extends Thread {
     share share;
     int producerNum;
-    public producer(share share, int producerNum)
-    {
-        this.share=share;
-        this.producerNum=producerNum;
+
+    public producer(share share, int producerNum) {
+        this.share = share;
+        this.producerNum = producerNum;
     }
+
     @Override
     public void run()
     {
-        try {
+        // try
+        // {
             Random random=new Random();
-            while (true)
+            synchronized(share){
+            while(true)
             {
-                if(share.data.size()>5)
-                {
-                    Thread.sleep(1);
-                }
-                else
-                {
-                    int value=random.nextInt(100);
-                    share.data.add(value);
-                    System.out.println(producerNum+" Produced --> "+value);
-                    Thread.sleep(10);
-                }
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+                
+                //     if(share.data.size()>=10){
+                //         Thread.sleep(10);
+                //     }
+                    // else{
+                        int value=random.nextInt(10);
+                        share.data[share.top++]=value;
+                        System.out.println(producerNum+" Produced --> "+value);
+                        // share.notifyAll();
+                        // Thread.sleep(500);
+                    }
+                        
+                 }
+            // }
+        // }
+        // catch (InterruptedException e)
+        // {
+        //     e.printStackTrace();
+        // }
     }
 }
