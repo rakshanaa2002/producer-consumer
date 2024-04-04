56f54c612fb632360cea3b3300e59b26d4d109fb
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-04-04 13:20:50 +0530

    share.java

--- share.java
+++ share.java
@@ -1,20 +1,22 @@
-import java.util.concurrent.ArrayBlockingQueue;
+// import java.util.concurrent.ArrayBlockingQueue;
 public class share
 {
-    ArrayBlockingQueue<Integer> data = new ArrayBlockingQueue<>(10);
+    // ArrayBlockingQueue<Integer> data=new ArrayBlockingQueue<>(10);
+    int[] data=new int[100];
+    int top=0;
     public static void main(String[] args)
     {
         share share=new share();
-        int producerNum=5;
-        int consumerNum=6;
+        int producerNum=3;
+        int consumeNum=6;
         for (int i=1;i<=producerNum;i++)
         {
             producer producer=new producer(share,i);
             producer.start();
         }
-        for (int i=1;i<=consumerNum;i++)
+        for (int i=1;i<=consumeNum;i++)
         {
-            consumer consumer=new consumer(share,i);
+            consumer consumer = new consumer(share,i);
             consumer.start();
         }
     }
