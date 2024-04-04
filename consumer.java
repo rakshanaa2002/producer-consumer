7e3e446661cb5ca15aa6c7946f9e46a56f4a76f9
rakshanaa2002 <133941940+rakshanaa2002@users.noreply.github.com>
2024-04-04 13:20:21 +0530

    consumer.java

--- consumer.java
+++ consumer.java
@@ -2,26 +2,32 @@ public class consumer extends Thread
 {
     share share;
     int consumerNum;
-    public consumer(share share, int consumerNum)
-    {
+
+    public consumer(share share, int consumerNum){
         this.share=share;
         this.consumerNum=consumerNum;
     }
+
     @Override
-    public void run()
-    {
-        try
-        {
-            while (true)
+    public void run() {
+        // try
+        // {
+            synchronized(share)
+                {
+            while (true) 
             {
-                while(share.data.isEmpty())
-                    Thread.sleep(1);
-                int value = share.data.remove();
-                System.out.println(consumerNum+" Consumed --> "+value);
-                Thread.sleep(1000);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+                
+                //     while(share.data.isEmpty())
+                //          share.wait();
+                    Integer value = share.data[share.top];
+                    System.out.println(consumerNum+" Consumed --> "+value+share.top);
+                    share.top--;
+        //             Thread.sleep(50);
+        //             share.notifyAll();
+                }
+        //     }
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
         }
     }
 }
